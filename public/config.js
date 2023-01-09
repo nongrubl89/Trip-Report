3b65dc36e3728ee280f67fbef0c485804b7535c8
Lisa <lisaburgnon@gmail.com>
2023-01-09 08:48:28 -0800

    removed api key

--- public/config.js
+++ public/config.js
@@ -1,3 +1,2 @@
 export const endpoint = `http://localhost:1337/graphql`;
 export const prodEndpoint = `fill me in when we deploy`;
-export const google_maps_key = "AIzaSyAiPePDIL-iGCiqkEol1mkYlBTWwxwzitk";
