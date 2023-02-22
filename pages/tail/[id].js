f3659271d81c8eabdcd0a3f1aac57c1247b44c36
nongrubl89 <lisaburgnon@gmail.com>
2023-02-22 15:13:00 -0800

    more deployment fixes

--- pages/tail/[id].js
+++ pages/tail/[id].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import TailPage from '../../components/SingleTailPage';
 
-export default function SingleTailPage({ query }) {
+export default function SingleTailPage() {
   const router = useRouter();
   console.log(router.query.id);
   return <TailPage id={router.query.id} />;
