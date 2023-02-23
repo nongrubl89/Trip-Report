9fd983c153f82b6b32645d3b88b7e94c830dc5bd
nongrubl89 <lisaburgnon@gmail.com>
2023-02-23 07:40:54 -0800

    redirect after edit

--- components/NewTrip.js
+++ components/NewTrip.js
@@ -164,7 +164,7 @@ export default function NewTrip({ tail }) {
             },
           });
           console.log(res);
-          // Router.push({ pathname: `/tail/${tail}` });
+          Router.push({ pathname: `/tail/${tail}` });
         }}
       >
         <fieldset>
